9beed89d061260f69c97736246cc0e152b4d95c3
meteor.you <meteor.you@hy-tech.com.tw>
2019-02-25 12:17:02 +0800

    add index.html

---
+++ precache-manifest.557f3672e16edc5ecac754996437dfc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "822383ff797e5040c37f",
+    "url": "/static/js/main.822383ff.chunk.js"
+  },
+  {
+    "revision": "3d6f562872a2df7d976b",
+    "url": "/static/js/2.3d6f5628.chunk.js"
+  },
+  {
+    "revision": "822383ff797e5040c37f",
+    "url": "/static/css/main.8adb9a4a.chunk.css"
+  },
+  {
+    "revision": "6696fb044f7a77d0dc41ef4fddf8f2b8",
+    "url": "/index.html"
+  }
+];
